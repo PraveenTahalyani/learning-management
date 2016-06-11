ee056e829d3e978893bf39787f597eda9f8c1fd0
Praveen <praveen.tahalyani>
2016-06-11 13:04:45 +0530

    added few gsp pages

--- grails-app/controllers/learning/management/FormController.groovy
+++ grails-app/controllers/learning/management/FormController.groovy
@@ -4,9 +4,16 @@ class FormController {
 
     def index() {}
     def saveuser(){
-        println"saveuser action executed"
-        println("parameters recieved")
-        redirect(controller:"form",action:"thankyou",params:[name:"you"])
+//        println"saveuser action executed"
+//        println("parameters recieved")
+//
+//   redirect(controller:"form",action:"thankyou",params:[name:"you"])
+   println params.firstName
+    println params.lastName
+        println params.email
+        //println params.password
+        println params.DOB
+        return[firstName:params.firstName,lastName:params.lastName,Email:params.email,currentDate:new Date(),age:100,DOB:params.DOB]
 
     }
     def thankyou()
